55efa0d6098a21175a294fbe97af65f8e930e47d
Thomas Birkenes <45704978+birken92@users.noreply.github.com>
2019-02-16 15:55:46 +1100

    v4.1-update

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Header from './Header';
+
+class App extends React.Component {
+  state = {
+    pageHeader: 'Naming Contests'
+  };
+  componentDidMount() {
+      console.log('did Mount');
+      debugged;
+  }
+  componentWillUnmount() {
+      console.log('will Unmount');
+        debugger;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header message={this.state.pageHeader} />
+        <div>
+          ...
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
